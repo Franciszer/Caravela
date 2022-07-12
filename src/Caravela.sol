f01ebc6ef092c2b9c97eddfea8f5e2b6d7db1a4a
Francois THIERREE <frthierr@e1r7p4.clusters.42paris.fr>
2022-07-12 19:46:43 +0200

    added batch make/take/cancel (untested)

--- src/Caravela.sol
+++ src/Caravela.sol
@@ -22,6 +22,12 @@ contract Caravela is Context, ERC1155Receiver {
 
     event CancelledERC1155Sale(bytes32 indexed uid, Order.ERC1155Sale order);
 
+    event MadeERC1155BatchSale(bytes32 indexed uid, Order.ERC1155BatchSale order);
+
+    event TookERC1155BatchSale(bytes32 indexed uid, Order.ERC1155BatchSale order);
+
+    event CancelledERC1155BatchSale(bytes32 indexed uid, Order.ERC1155BatchSale order);
+
     /// @dev make ERC1155 sale
     /// @param order order
     function make_sale(Order.ERC1155Sale memory order) external {
@@ -50,6 +56,36 @@ contract Caravela is Context, ERC1155Receiver {
         orders[uid] = true;
 
         emit MadeERC1155Sale(uid, order);
+    }
+
+    /// @dev make ERC1155 sale
+    /// @param order order
+    function make_batch_sale(Order.ERC1155BatchSale memory order) external {
+        bytes32 uid = Order.compute_batch_sale_uid(order);
+
+        require(orders[uid] == false, "make_batch_sale: order is already active");
+
+        require(order.kind == Order.Kind.batch_sale, "make_batch_sale: order is not a sale");
+
+        require(
+            _msgSender() == order.emitter,
+            "make_batch_sale: emitter is not the owner of the token"
+        );
+
+        IERC1155 collection = order.collection;
+
+        require(
+            collection.isApprovedForAll(_msgSender(), address(this)),
+            "make_batch_sale: marketplace contract is not approved to handle tokens"
+        );
+
+        collection.safeBatchTransferFrom(
+            _msgSender(), address(this), order.ids, order.values, new bytes(0x0)
+        );
+
+        orders[uid] = true;
+
+        emit MadeERC1155BatchSale(uid, order);
     }
 
     /// @dev take ERC1155 sale
@@ -83,6 +119,37 @@ contract Caravela is Context, ERC1155Receiver {
         emit TookERC1155Sale(uid, order);
     }
 
+    /// @dev take ERC1155 sale
+    /// @param order order
+    function take_batch_sale(Order.ERC1155BatchSale memory order) external {
+        bytes32 uid = Order.compute_batch_sale_uid(order);
+
+        require(orders[uid] == true, "take_sale: order is not active");
+        require(order.kind == Order.Kind.batch_sale, "take_sale: order is not a sale");
+
+        IERC20 currency = order.currency;
+
+        require(
+            currency.allowance(_msgSender(), address(this)) >= order.price,
+            "take_order: marketplace contract allowance is too small to buy token"
+        );
+
+        bool transaction_succeeded =
+            currency.transferFrom(_msgSender(), order.emitter, order.price);
+
+        require(
+            transaction_succeeded == true, "take_sale: ERC20 token transfer failed"
+        );
+
+        order.collection.safeBatchTransferFrom(
+            address(this), _msgSender(), order.ids, order.values, new bytes(0x0)
+        );
+
+        orders[uid] = false;
+
+        emit TookERC1155BatchSale(uid, order);
+    }
+
     /// @dev cancel ERC1155 sale
     /// @param order order
     function cancel_sale(Order.ERC1155Sale memory order) external {
@@ -106,6 +173,31 @@ contract Caravela is Context, ERC1155Receiver {
         orders[uid] = false;
 
         emit CancelledERC1155Sale(uid, order);
+    }
+
+    /// @dev cancel ERC1155 sale
+    /// @param order order
+    function cancel_batch_sale(Order.ERC1155BatchSale memory order) external {
+        bytes32 uid = Order.compute_batch_sale_uid(order);
+
+        require(orders[uid] == true, "cancel_sale: order is not active");
+
+        require(
+            order.kind == Order.Kind.batch_sale, "cancel_sale: order is not a sale"
+        );
+
+        require(
+            order.emitter == _msgSender(),
+            "cancel_sale: you are not emitter of this sale"
+        );
+
+        order.collection.safeBatchTransferFrom(
+            address(this), order.emitter, order.ids, order.values, new bytes(0x0)
+        );
+
+        orders[uid] = false;
+
+        emit CancelledERC1155BatchSale(uid, order);
     }
 
     function onERC1155Received(
