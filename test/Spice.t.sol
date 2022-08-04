99e3541e4d18b8e7921a91b546d177f11fb049cf
Franciszer <ftthierree@gmail.com>
2022-08-04 18:18:43 +0200

    ERC1155 base logic

--- test/Spice.t.sol
+++ test/Spice.t.sol
@@ -2,29 +2,56 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "src/Caravela.sol";
+import "src/SigUtils.sol";
 import "src/Spice.sol";
 
-contract ContractTest is Test {
-    Spice public currency;
-    uint256 constant INITIAL_BALANCE = 0xFFFF;
-    address public admin;
+contract SpiceTest is Test {
+    Spice internal spice;
+    SigUtils internal sigUtils;
+
+    uint256 internal ownerPrivateKey;
+    uint256 internal spenderPrivateKey;
+
+    address internal owner;
+    address internal spender;
 
     function setUp() public {
-        admin = address(this);
+        spice = new Spice(address(this), "name", "symbol", 18);
+        sigUtils = new SigUtils(spice.DOMAIN_SEPARATOR());
 
-        currency = new Spice("name", "symbol");
+        ownerPrivateKey = 0xA11CE;
+        spenderPrivateKey = 0xB0B;
 
-        currency.mint(admin, INITIAL_BALANCE);
+        owner = vm.addr(ownerPrivateKey);
+        spender = vm.addr(spenderPrivateKey);
+
+        spice.mint(owner, 1e18);
     }
 
-    function testMint() public {
-        uint256 expected_balance = INITIAL_BALANCE;
+    function test_Permit() public {
+        SigUtils.Permit memory permit = SigUtils.Permit({
+            owner: owner,
+            spender: spender,
+            value: 1e18,
+            nonce: 0,
+            deadline: 1 days
+        });
 
-        assertEq(expected_balance, currency.balanceOf(admin));
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        spice.permit(
+            permit.owner,
+            permit.spender,
+            permit.value,
+            permit.deadline,
+            v,
+            r,
+            s
+        );
+
+        assertEq(spice.allowance(owner, spender), 1e18);
+        assertEq(spice.nonces(owner), 1);
     }
-
-	function testTransferPermit() public {
-		
-	}
 }
